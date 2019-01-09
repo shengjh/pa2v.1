db24f3f738e464e9b8370ab8e97ec23714b627a4
tracer-ics2018 <tracer@njuics.org>
2019-01-09 05:14:23 +0000

    >  compile U201514731 sherwin Linux 7a1b64cc9904 4.4.0-141-generic #167~14.04.1-Ubuntu SMP Mon Dec 10 13:20:24 UTC 2018 x86_64 GNU/Linux  05:14:22 up 17:30, 10 users,  load average: 0.44, 0.30, 0.16 1dae8a49835fc6cbfbc16ca73949c0b7a85dff56

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -77,6 +77,7 @@ make_EHelper(movsx) {
 	printf("******%x\n",cpu.eax);
 	id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
 	rtl_sext(&t0, &id_src->val, id_src->width);
+	printf("$$$%x %d\n",t0,id_dest->width);
 	operand_write(id_dest, &t0);
 	 print_asm_template2(movsx);
  	printf("******%x\n",cpu.eax);
